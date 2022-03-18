d038949ee912d07c861bf246133f6388e675cf86
Ori <isinglever@gmail.com>
2022-03-18 16:12:54 +0800

    add user logout

--- imooc-api/src/main/java/i/mooc/controller/PassportController.java
+++ imooc-api/src/main/java/i/mooc/controller/PassportController.java
@@ -4,12 +4,17 @@ import i.mooc.pojo.Users;
 import i.mooc.pojo.bo.UserBO;
 import i.mooc.service.StuService;
 import i.mooc.service.UserService;
+import i.mooc.utils.CookieUtils;
 import i.mooc.utils.IMOOCJSONResult;
+import i.mooc.utils.JsonUtils;
 import i.mooc.utils.MD5Utils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 //@Controller
 @RestController
@@ -32,7 +37,9 @@ public class PassportController {
     }
     
     @PostMapping("/login")
-    public IMOOCJSONResult login(@RequestBody UserBO userBO) throws Exception{
+    public IMOOCJSONResult login(@RequestBody UserBO userBO, 
+                                 HttpServletRequest request, 
+                                 HttpServletResponse response) throws Exception{
         String username = userBO.getUsername();
         String password = userBO.getPassword();
         
@@ -42,14 +49,16 @@ public class PassportController {
             return IMOOCJSONResult.errorMsg("username or password can't be null");
         }
         
-        
-        
         Users userResult = userService.queryUserForLogin(username, MD5Utils.getMD5Str(password));
         
         if (userResult == null) return IMOOCJSONResult.errorMsg("username or password is incorrect");
         
         userResult.setPassword(null);
         userResult = setNullProperty(userResult);
+
+
+        CookieUtils.setCookie(request, response, "user",
+                JsonUtils.objectToJson(userResult), true);
         return IMOOCJSONResult.ok(userResult);
 
 
@@ -57,7 +66,7 @@ public class PassportController {
     // TODO 生成用户token，存入redis会话
     // TODO 同步购物车数据
 
-}
+    }
 
     private Users setNullProperty(Users userResult) {
         userResult.setPassword(null);
@@ -70,7 +79,9 @@ public class PassportController {
     }
 
     @PostMapping("/regist")
-    public IMOOCJSONResult regist(@RequestBody UserBO userBO) {
+    public IMOOCJSONResult regist(@RequestBody UserBO userBO, 
+                                  HttpServletResponse response, 
+                                  HttpServletRequest request) {
         String username = userBO.getUsername();
         String password = userBO.getPassword();
         String confirmPassword = userBO.getConfirmPassword();
@@ -89,7 +100,19 @@ public class PassportController {
 
         if (!password.equals(confirmPassword)) return IMOOCJSONResult.errorMsg("password does not match");
 
-        userService.creatUser(userBO);
+        Users userResult = userService.creatUser(userBO);
+        CookieUtils.setCookie(request, response, "user",
+                JsonUtils.objectToJson(userResult), true);
+        
+        return IMOOCJSONResult.ok();
+    }
+    
+    @PostMapping("/logout")
+    public IMOOCJSONResult logout(@RequestParam String userId, 
+                                  HttpServletResponse response, 
+                                  HttpServletRequest request) {
+        
+        CookieUtils.deleteCookie(request, response, "user");
         return IMOOCJSONResult.ok();
     }
 }
